01453de50e4ba49e2d307e563a96a50d41154b56
Iulian Rotaru <iulian@gnotis.ro>
2021-07-21 14:21:40 +0300

    Cleanup deprecated methods from CLI console.

--- cli/src/main/java/com/jslib/docli/Console.java
+++ cli/src/main/java/com/jslib/docli/Console.java
@@ -1,11 +1,8 @@
 package com.jslib.docli;
 
-import static java.lang.String.format;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Map;
 
 import org.fusesource.jansi.AnsiConsole;
 
@@ -13,90 +10,16 @@ import com.jslib.dospi.IConsole;
 import com.jslib.dospi.IFormatter;
 import com.jslib.dospi.UserCancelException;
 
-import js.util.Params;
+import js.lang.BugError;
 
 public class Console implements IConsole {
-	private static final String LINE_END = "\r\n";
+	private static final BufferedReader STDIN = new BufferedReader(new InputStreamReader(System.in));
 
 	public Console() {
 		AnsiConsole.systemInstall();
 	}
 
-	public void alert(String format, Object... args) {
-		System.out.printf(format, args);
-		System.out.println();
-	}
-
-	public void alert(Map<String, String> definitionsList) {
-		int keyWidth = 0;
-		for (String key : definitionsList.keySet()) {
-			if (keyWidth < key.length()) {
-				keyWidth = key.length();
-			}
-		}
-		String message = format(" - %%-%ds : %%s%s", keyWidth, LINE_END);
-
-		for (Map.Entry<String, String> entry : definitionsList.entrySet()) {
-			System.out.printf(message, entry.getKey(), entry.getValue());
-		}
-	}
-
-	private static final int CR = 13;
-	private static final int LF = 10;
-	private static final int CTRL_C = -1;
-
-	public String input(String message, String... defaultValue) throws IOException, InterruptedException {
-		System.out.print("- ");
-		System.out.print(message);
-		System.out.print(": ");
-		if (defaultValue.length == 1) {
-			System.out.printf("[%s]: ", defaultValue[0]);
-		}
-
-		StringBuilder builder = new StringBuilder();
-		int c;
-		while ((c = System.in.read()) != LF) {
-			// System.out.printf("%d\n", c);
-			if (c == CR) {
-				continue;
-			}
-			if (c == CTRL_C) {
-				throw new InterruptedException();
-			}
-			builder.append((char) c);
-		}
-
-		String value = builder.toString();
-		if (value.isEmpty() && defaultValue.length == 1) {
-			value = defaultValue[0];
-		}
-		return value;
-	}
-
-	private static final BufferedReader STDIN = new BufferedReader(new InputStreamReader(System.in));
-
-	public boolean confirm(String warning) throws IOException {
-		System.out.println();
-		System.out.print(warning);
-		System.out.print(": yes | [no]: ");
-		String answer = STDIN.readLine();
-		System.out.println();
-		return answer.equalsIgnoreCase("yes");
-	}
-
 	@Override
-	public void confirm(String format, Object... args) throws IOException, UserCancelException {
-		System.out.println();
-		String action = String.format(format, args);
-		System.out.print(action);
-		System.out.print(": yes | [no]: ");
-		String answer = STDIN.readLine();
-		System.out.println();
-		if (!answer.equalsIgnoreCase("yes")) {
-			throw new UserCancelException(action);
-		}
-	}
-
 	public void print(String format, Object... args) {
 		System.out.printf(format, args);
 		System.out.println();
@@ -129,7 +52,45 @@ public class Console implements IConsole {
 		System.out.println(formatter.format(object));
 	}
 
+	public String input(String message, String... defaultValue) {
+		System.out.print("- ");
+		System.out.print(message);
+		System.out.print(": ");
+		if (defaultValue.length == 1) {
+			System.out.printf("[%s]: ", defaultValue[0]);
+		}
+
+		String value = readLine();
+		if (value.isEmpty() && defaultValue.length == 1) {
+			value = defaultValue[0];
+		}
+		return value;
+	}
+
+	@Override
+	public void confirm(String format, Object... args) throws UserCancelException {
+		System.out.println();
+		String action = String.format(format, args);
+		System.out.print(action);
+		System.out.print(": yes | [no]: ");
+		
+		String answer = readLine();
+		System.out.println();
+		
+		if (!answer.equalsIgnoreCase("yes")) {
+			throw new UserCancelException(action);
+		}
+	}
+
 	public void crlf() {
 		System.out.println();
+	}
+
+	private static String readLine() {
+		try {
+			return STDIN.readLine();
+		} catch (IOException e) {
+			throw new BugError(e);
+		}
 	}
 }
--- spi/src/main/java/com/jslib/dospi/IConsole.java
+++ spi/src/main/java/com/jslib/dospi/IConsole.java
@@ -1,13 +1,18 @@
 package com.jslib.dospi;
 
-import java.io.IOException;
-
 public interface IConsole {
 
 	void print(String format, Object... args);
-	
+
 	<T> void print(IFormatter<T> formatter, T object);
-	
-	void confirm(String format, Object... args) throws IOException, UserCancelException;
-	
+
+	/**
+	 * Display action message to user and wait for confirmation. Throws exception if user denies the action.
+	 * 
+	 * @param format action formatted message,
+	 * @param args optional format arguments.
+	 * @throws UserCancelException if user denies the action.
+	 */
+	void confirm(String format, Object... args) throws UserCancelException;
+
 }
